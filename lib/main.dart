98787a64d720f9abc7326ee875ad141be3ba49e0
Valen-Rossi <valerossi2004@gmail.com>
2023-11-27 11:42:43 -0300

    providers started

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,15 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async{
 
   await dotenv.load(fileName: '.env');
 
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(child: MainApp())
+  );
 }
 
 class MainApp extends StatelessWidget {
