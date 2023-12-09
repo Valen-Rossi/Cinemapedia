884fd4d48e5d532cf365d738a06337f0706f41df
Valen-Rossi <valerossi2004@gmail.com>
2023-12-09 10:35:32 -0300

    statefulshellroute branches cuasi terminados (falta crear categories y posible error caso de login)

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -7,38 +7,61 @@ final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
 
-    ShellRoute(
+    StatefulShellRoute.indexedStack(
       builder: (context, state, child){
         return HomeScreen(childView: child);
       },
-      routes: [
+      branches: [
 
-        GoRoute(
-          path:'/',
-          builder: (context, state){
-            return const HomeView();
-          },
-          routes: [
-
+        StatefulShellBranch(
+          routes: [     
             GoRoute(
-              path: 'movie/:id',
-              name: MovieScreen.name,
-              builder: (context, state) {
-
-                final movieId = state.pathParameters['id']?? 'no-id';
-
-                return MovieScreen(movieId: movieId);
+              path:'/',
+              builder: (context, state){
+                return const HomeView();
               },
-            ),
-            
+              routes: [
+
+                GoRoute(
+                  path: 'movie/:id',
+                  name: MovieScreen.name,
+                  builder: (context, state) {
+
+                    final movieId = state.pathParameters['id']?? 'no-id';
+
+                    return MovieScreen(movieId: movieId);
+                  },
+                ),
+                
+              ]
+            )
           ]
         ),
+        
+        StatefulShellBranch(
+          routes:[
+            
+            GoRoute(
+              path:'/categories',
+              builder: (context, state){
+                return const FavoritesView();
+              }
+            )
 
-        GoRoute(
-          path:'/favorites',
-          builder: (context, state){
-            return const FavoritesView();
-          }
+          ] 
+        ),
+
+        StatefulShellBranch(
+          routes:[
+            
+            GoRoute(
+              path:'/favorites',
+              builder: (context, state){
+                return const FavoritesView();
+              }
+            )
+
+          ] 
         ),
 
       ],
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -2,46 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
 
-  int getCurrentIndex(BuildContext context){
-    final String location= GoRouterState.of(context).matchedLocation;
+  final StatefulNavigationShell childView;
 
-    switch (location) {
-      case '/':
-        return 0;
-      case '/categories':
-        return 1;
-      case '/favorites':
-        return 2;
-      default:
-        return 0;
-    
-    }
-  }
-
-  void onItemTap(BuildContext context, int index){
-
-    switch (index) {
-      case 0:
-        context.go('/');
-        break;
-      case 1:
-        context.go('/');
-        break;
-      case 2:
-        context.go('/favorites');
-        break;
-    }
-
-  }
+  const CustomBottomNavigation({super.key, required this.childView});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
-      currentIndex: getCurrentIndex(context),
-      onTap: (index) => onItemTap(context, index),
+      currentIndex: childView.currentIndex,
+      onTap: (index) => childView.goBranch(index),
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max),
